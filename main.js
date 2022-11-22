9b6e406df43cb2c29a9054feb4e5badcb4664db5
Suresh474565 <85395556+Suresh474565@users.noreply.github.com>
2022-11-22 14:52:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+objectDetector="";
+
 function setup()
 {
 
@@ -5,6 +7,7 @@ canvas= createCanvas(600,500);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
+objectDetector= ml5.objectDetector("cocossd",modelLoaded);
 document.getElementById("status").innerHTML= "status- Detecting Objects";
 
 }
@@ -15,4 +18,31 @@ function draw()
 image(video,0,0,600,500);
 fill('#FF0000');
 stroke('#8456e8');
+}
+
+function modelLoaded()
+{
+
+console.log("Model is loaded");
+status = true;
+objectDetector.detect(img,gotResult);
+
+
+}
+
+function gotResult(error,results)
+{
+
+if(error)
+{
+
+console.log(error); 
+
+}
+  
+console.log(results);
+
+objects = results;
+
+
 }
